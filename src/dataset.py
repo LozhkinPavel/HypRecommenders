7b9a116dff1d13ac2ddb2a99a8390bb6739f3e40
Pavel Lozhkin <lozhkin.pavel@gmail.com>
2026-02-16 05:39:02 +0300

    fix

--- src/dataset.py
+++ src/dataset.py
@@ -60,7 +60,7 @@ class UserItemWithNegativesDataset(Dataset):
 
         self.users_interactions_df = interactions_df.groupby(by='user_id').agg({'item_id': list})
         self.items_interactions_df = interactions_df.groupby(by='item_id').agg({'user_id': list})
-        self.interactions = torch.sparse_coo_tensor(np.stack((interactions_df['user_id'].values, interactions_df['item_id'].values), axis=0), ones, (self.num_users, self.num_items))
+        self.interactions = torch.sparse_coo_tensor(np.stack((interactions_df['user_id'].values, interactions_df['item_id'].values), axis=0), ones, (self.num_users, self.num_items)).to_dense()
         self.num_negatives = num_negatives
 
         if shuffle:
@@ -79,7 +79,7 @@ class UserItemWithNegativesDataset(Dataset):
         # items_interactions = torch.zeros((self.num_negatives + 1, self.num_users), dtype=torch.float64)
         # user_inds = torch.tensor(self.items_interactions_df.loc[[pos_item_id, *neg_item_id.tolist()], 'user_id'], dtype=torch.int64)
         # items_interactions.index_add(1, user_inds, torch.ones(user_inds.shape[0], dtype=torch.float64))
-        items_interactions = torch.index_select(self.interactions, dim=1, index=torch.tensor([pos_item_id, *neg_item_id.tolist()])).to_dense().T
+        items_interactions = self.interactions[:, torch.tensor([pos_item_id, *neg_item_id.tolist()])].T
 
         return (user_interactions, items_interactions), ()
 
